1ad364302d743c87370e113103a7987f364632b4
Arm7711 <armmooooooo@gmail.com>
2026-05-10 22:49:35 +0400

    final 6

--- src/hooks/DisableIOSZoom.jsx
+++ src/hooks/DisableIOSZoom.jsx
@@ -1,36 +1,29 @@
-import { useEffect } from 'react'
+import { useLayoutEffect } from 'react';
 
-export function DisableIOSZoom() {
-    useEffect(() => {
-        const preventGesture = e => e.preventDefault()
+export function useDisableZoom() {
+    useLayoutEffect(() => {
+        const preventGesture = (e: Event) => {
+            e.preventDefault();
+        };
 
-        document.addEventListener('gesturestart', preventGesture, { passive: false })
-        document.addEventListener('gesturechange', preventGesture, { passive: false })
-        document.addEventListener('gestureend', preventGesture, { passive: false })
-
-        let lastTouchEnd = 0
-
-        const preventDoubleTapZoom = e => {
-            const now = Date.now()
-
-            if (now - lastTouchEnd <= 300) {
-                e.preventDefault()
-            }
-
-            lastTouchEnd = now
-        }
-
-        document.addEventListener('touchend', preventDoubleTapZoom, {
+        document.addEventListener('gesturestart', preventGesture, {
             passive: false,
-        })
+        });
+
+        document.addEventListener('gesturechange', preventGesture, {
+            passive: false,
+        });
+
+        document.addEventListener('gestureend', preventGesture, {
+            passive: false,
+        });
 
         return () => {
-            document.removeEventListener('gesturestart', preventGesture)
-            document.removeEventListener('gesturechange', preventGesture)
-            document.removeEventListener('gestureend', preventGesture)
-            document.removeEventListener('touchend', preventDoubleTapZoom)
-        }
-    }, [])
+            document.removeEventListener('gesturestart', preventGesture);
 
-    return null
+            document.removeEventListener('gesturechange', preventGesture);
+
+            document.removeEventListener('gestureend', preventGesture);
+        };
+    }, []);
 }
